6e8a0e237426dae76a7eeafeb020166b00629b8a
ankush mondal <mondalankush9851@gmail.com>
2024-09-28 19:24:23 +0530

    all

--- bit/main.cc
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
-int main()
-{
-    const std::uint16_t i = 11101;
-    std::cout << "rotl(i,5)  = " << std::bitset<5>(std::rotl(i, 1)) << '\n';
-}
